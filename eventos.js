2d4387f1c3ce3d0b75ea31f1a7ed99ba7e6dba8f
beluarias <belenariasfor@gmail.com>
2023-08-29 20:50:02 -0300

    solucion

--- eventos.js
+++ eventos.js
@@ -1,11 +1,14 @@
-function mostrarAlerta() {
-    alert("¡Hola!");
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     const divbutton = document.getElementById('divbutton');
+    const button = document.getElementById('button');
 
     divbutton.addEventListener('click', () => {
-        alert('Hola! Soy el div');
+        alert('¡Hola! Soy el div');
+    });
+
+    button.addEventListener('click', (event) => {
+        event.stopPropagation(); // Detiene la propagación del evento del botón
+        alert('Hola! Soy el botón');
     });
 });
+
